7a849a8de7d7b2e0bd281f6c8f0a0a267ed360b0
Soham Choudhury <soham874@gmail.com>
2022-05-29 16:40:28 +0530

    Added : Read function
    Service to fetch list of all users in database

---
+++ target/classes/com/PersonService.java
@@ -0,0 +1,49 @@
+package com;
+
+import java.util.*;
+import com.MongoFactory;
+
+import org.apache.log4j.Logger;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mongodb.DBCollection;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+
+@Service("personService")
+@Transactional
+public class PersonService {
+	
+	private static String db_name = "Cecncus";
+	private static String collection_name = "CRUD-Operation-Spring-MVC";
+	
+	private static Logger log = Logger.getLogger(PersonService.class);
+	
+	public List<Person> getAll(){
+		
+		List<Person> = new ArrayList<>();
+		DBCollection coll = MongoFactory.getCollection(db_name,collection_name);
+		DBCursor cursor = coll.find(); 
+		
+        while(cursor.hasNext()) {           
+            DBObject dbObject = cursor.next();
+ 
+            Person person = new Person();
+            person.setId(dbObject.get("id").toString());
+            person.setName(dbObject.get("Name").toString());
+            person.setGender(dbObject.get("Gender").toString());
+            person.setLocation(dbObject.get("Location").toString());
+            person.setAge(Integer.parseInt(dbObject.get("Age")));
+ 
+            // Adding the user details to the list.
+            user_list.add(user);
+        }
+        log.debug("Total records fetched from the mongo database are= " + user_list.size());
+        return user_list;
+	}
+	
+}
